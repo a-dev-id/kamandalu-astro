a9ee3c3b9319ebb90ffdbd9260612b5b79c6370a
A-Dev <3xasov@gmail.com>
2025-06-11 13:41:21 +0800

    final

--- src/scripts/splide-init.js
+++ src/scripts/splide-init.js
@@ -1,24 +1,23 @@
+import "@splidejs/splide/css";
 import Splide from "@splidejs/splide";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const splide = new Splide("#card-carousel", {
-    type: "loop",
-    perPage: 2,
-    gap: "4rem",
-    speed: 800,
-    easing: "ease-in-out",
-    arrows: false,
-    pagination: false,
-    breakpoints: {
-      768: {
-        perPage: 1,
-      },
+const splide = new Splide("#card-carousel", {
+  type: "loop",
+  perPage: 2,
+  gap: "4rem",
+  speed: 800,
+  easing: "ease-in-out",
+  arrows: false,
+  pagination: false,
+  breakpoints: {
+    768: {
+      perPage: 1,
     },
-  });
-
-  splide.mount();
-
-  // ✅ Link bottom buttons to the slider
-  document.getElementById("custom-prev").addEventListener("click", () => splide.go("<"));
-  document.getElementById("custom-next").addEventListener("click", () => splide.go(">"));
+  },
 });
+
+splide.mount();
+
+// ✅ Link bottom buttons to the slider
+document.getElementById("custom-prev").addEventListener("click", () => splide.go("<"));
+document.getElementById("custom-next").addEventListener("click", () => splide.go(">"));
