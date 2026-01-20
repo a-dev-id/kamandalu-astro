cabb78cedc0429c713360badda5b9487f4b4356d
A-Dev <3xasov@gmail.com>
2026-01-20 16:58:20 +0800

    add sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://kamandaluresort.com",
   output: "static",
-  outDir: "./public_html", // ✅ correct place
+  outDir: "./public_html",
   build: {
     format: "directory",
   },
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+  ],
 });
